e5dc722e54d9fed3e78533aec478fc1356d34390
Ashley <ajspygirl08@gmail.com>
2021-10-27 18:52:53 -0400

    fixed issue of disappearing cards -- now all 52 cards stay in play

--- war.py
+++ war.py
@@ -19,22 +19,20 @@ def check_cards(player_card, dealer_card):
     else:
         return 'war'
 
-def go_to_war(player_hand, dealer_hand):
+def go_to_war(player_card, dealer_card, player_hand, dealer_hand):
     """
     if cards are equal, place down three extra cards and flip over the fourth card
     highest fourth card wins all cards on table
     """
     print('------------ WAR!! ------------')
-    # cards that are about to be drawn
-    player_table_cards = []
-    dealer_table_cards = []
+    # fourth card -- will be flipped over
     player_fourth = []
     dealer_fourth = []
 
     # draws 4 cards and puts them 'on the table' (into the table cards list)
     for _ in range(3):
-        player_hand, player_table_cards = cards.top_draw(player_hand, player_table_cards)
-        dealer_hand, dealer_table_cards = cards.top_draw(dealer_hand, dealer_table_cards)
+        player_hand, player_card = cards.top_draw(player_hand, player_card)
+        dealer_hand, dealer_card = cards.top_draw(dealer_hand, dealer_card)
 
     # get the fourth card (this card will be 'flipped over' to determine who wins war)
     player_hand, player_fourth = cards.top_draw(player_hand, player_fourth)
@@ -46,33 +44,55 @@ def go_to_war(player_hand, dealer_hand):
     winner = check_cards(player_fourth, dealer_fourth)
 
     # adds the fourth card to the table cards so all can be added to the 
-    player_table_cards.append(player_fourth)
-    dealer_table_cards.append(dealer_fourth)
+    player_card.append(player_fourth[0])
+    dealer_card.append(dealer_fourth[0])
+
+    print('player cards on table:', player_card, 'dealer cards on table', dealer_card)
 
     # winner_check() where war_bool = True
-    winner_check(winner, player_table_cards, dealer_table_cards, player_hand, dealer_hand, war_bool=False)
+    winner_check(winner, player_card, dealer_card, player_hand, dealer_hand, True)
 
-def take_winnings(winner, player_card, dealer_card, winner_hand, war_bool=False):
+def take_winnings(winner, player_card, dealer_card, player_hand, dealer_hand, war_bool):
     """
     takes both cards and adds it to winner's hand
     """
-    if war_bool == False:
-        # adds both cards to bottom of winner hand
-        winner_hand.append(player_card)
-        winner_hand.append(dealer_card)
-
-        # prints winner
-        print('Player card:', player_card[0][3], '\nDealer card:', dealer_card[0][3])
-        print('Winner:', winner)
-
-    # war_bool is True
-    else:
-        # player_card/dealer_card is now not just one card, but a list of 4 cards, so iterate over all to add to winner hand
-        for index in range(len(player_card)):
-            winner_hand.append(player_card[index])
-            winner_hand.append(dealer_card[index])
+    print(war_bool)
+    # this function is being called after a 'war' has taken place
+    if war_bool == True:
+        print('player cards to be added',len(player_card), player_card)
+        print('dealer cards to be added',len(dealer_card), dealer_card)
+        if winner == 'player':
+            # player_card/dealer_card is now not just one card, but a list of 4 cards, so iterate over all to add to winner hand
+            for index in range(len(player_card)):
+                print('adding to player hand:', dealer_card[index])
+                player_hand.append(player_card[index])
+                print('adding to player hand:', dealer_card[index])
+                player_hand.append(dealer_card[index])
+        else:
+            # player_card/dealer_card is now not just one card, but a list of 4 cards, so iterate over all to add to winner hand
+            for index in range(len(player_card)):
+                print('adding to dealer hand:', player_card[index])
+                dealer_hand.append(player_card[index])
+                print('adding to dealer hand:', dealer_card[index])
+                dealer_hand.append(dealer_card[index])
         
         # prints winner
+        print('-- war round --')
+        print('Player card:', player_card[0][3], '\nDealer card:', dealer_card[0][3])
+        print('Winner:', winner)
+    # normal round has taken place
+    else:
+        if winner == 'player':
+            # adds both cards to bottom of player hand
+            player_hand.append(player_card)
+            player_hand.append(dealer_card)
+        else:
+            # adds both cards to bottom of dealer hand
+            dealer_hand.append(player_card)
+            dealer_hand.append(dealer_card)
+
+        # prints winner
+        print('-- normal round --')
         print('Player card:', player_card[0][3], '\nDealer card:', dealer_card[0][3])
         print('Winner:', winner)
 
@@ -82,12 +102,11 @@ def winner_check(winner, player_card, dealer_card, player_hand, dealer_hand, war
     """
     based on what check_cards returns, add the cards to winner's hand OR go to war
     """
-    if winner == 'player':
-        take_winnings(winner, player_card, dealer_card, player_hand, war_bool)
-    elif winner == 'dealer':
-        take_winnings(winner, player_card, dealer_card, dealer_hand, war_bool)
+    if winner == 'player' or winner == 'dealer':
+        take_winnings(winner, player_card, dealer_card, player_hand, dealer_hand, war_bool)
+    # if winner is not player or dealer, then must go to war
     else:
-        go_to_war(player_hand, dealer_hand)
+        go_to_war(player_card, dealer_card, player_hand, dealer_hand)
 
 def check_finish(player_hand, dealer_hand):
     """
@@ -118,9 +137,21 @@ def main():
     for card in deck:
         print(card[3], card)
 
-    deck, player_hand, dealer_hand = cards.deal(deck, 21)
+    print(len(deck))
 
-    print(len(player_hand), len(dealer_hand), len(deck))
+    # deck, player_hand, dealer_hand = cards.deal(deck, 21)
+    player_hand, dealer_hand = cards.cut(deck)
+
+    # print(len(player_hand), len(dealer_hand), len(deck))
+    # print('deck:' )
+    # for card in deck:
+    #     print(card[3], card)
+    # print('player:' )
+    # for card in player_hand:
+    #     print(card[3], card)
+    # print('dealer:' )
+    # for card in dealer_hand:
+    #     print(card[3], card)
 
     # variable to ensure that game ends when either player or dealer runs out of cards
     still_playing = True
@@ -136,6 +167,9 @@ def main():
         player_hand, player_card = cards.top_draw(player_hand, player_card)
         dealer_hand, dealer_card = cards.top_draw(dealer_hand, dealer_card)
 
+        
+        print('player card: ', player_card, 'dealer card:', dealer_card)
+
         # check which card is higher - if cards are equal: 'go to war'
         result = check_cards(player_card, dealer_card)
         winner_check(result, player_card, dealer_card, player_hand, dealer_hand)
@@ -143,6 +177,8 @@ def main():
         
         print('Cards in player hand:', str(len(player_hand)))
         print('Cards in dealer hand:', str(len(dealer_hand)))
+
+        print('cards on table:', len(player_hand) + len(dealer_hand))
 
         # continue until one player has all 52 cards
         still_playing = check_finish(player_hand, dealer_hand)
@@ -158,5 +194,4 @@ if __name__ == '__main__':
 improvements: 
 
 solve if double, triple, etc war
-timing (so everything doesnt just appear at once)
 '''
